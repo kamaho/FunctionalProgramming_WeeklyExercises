d77ca10b56b8c442a230ca2e092060509453255d
Karl Martin Holst <99191682+kamaho@users.noreply.github.com>
2022-09-22 23:47:45 +0200

    Fixed duplicateAll, added description

--- Week38Exercise0.hs
+++ Week38Exercise0.hs
@@ -1,12 +1,14 @@
 module Week38Exercise0 where
+
     -- I denne filen skal du bruke rekursjon til å 
     -- lage funksjonen duplicateAll :: [a] -> [(a,a)] 
     -- som tar en liste og dupliserer alle elementene i listen.
-    -- For eksempel, duplicateAll [1,2,3] skal returnere [(1,1),(2,2),(3,3)].
-
+    
     duplicateAll :: [a] -> [(a,a)]
     duplicateAll [] = []
-    duplicateAll [x] = zip[x][x] 
+    duplicateAll (x:xs) = (x, x) : duplicateAll (xs)
+        
+     -- For eksempel, duplicateAll [1,2,3] skal returnere [(1,1),(2,2),(3,3)]
 
 
     
